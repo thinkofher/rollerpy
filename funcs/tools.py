5f0781cdd5340c2dffa3fe5728ba9e94f4292090
Beniamin <piaskupiasku@gmail.com>
2019-01-04 23:22:47 +0100

    implemented curveByCurve function

--- funcs/tools.py
+++ funcs/tools.py
@@ -2,9 +2,17 @@ import numpy as np
 from scipy.misc import derivative
 
 
+_GLOBALSYSTEM = (
+    np.array([1, 0, 0]),
+    np.array([0, 1, 0]),
+    np.array([0, 0, 1])
+)
+
+
 def trackTransitonCurve(point1, slope1, point2, slope2, n=100):
         '''
-        Creates u^3 paraboal from point1 to point2 with given slopes.
+        Creates u^3 paraboal [x, y, z] vector from
+        point1 to point2 with given slopes.
         '''
         t = np.linspace(0, 1, n)
         M = np.matrix(
@@ -71,5 +79,55 @@ def frenetFromPfuncs(t, xfunc, yfunc, zfunc):
 
     return (p, n, b)
 
-# TODO: Implement curvebycurve
+
+def _calcCos(veca, vecb):
+    '''
+    Calculating cos between veca and vecb.
+    '''
+    return np.dot(veca, vecb)/(
+        np.linalg.norm(veca)*np.linalg.norm(vecb)
+    )
+
+
+def curveByCurve(linspace, basefuncs, curfuncs):
+    '''
+    Returns [x, y, z] vector of curfuncs curve in local
+    coordinate system of basefuncs in given linspace.
+    '''
+    xnew = np.array([])
+    ynew = np.array([])
+    znew = np.array([])
+    xb, yb, zb = _GLOBALSYSTEM
+    for t in linspace:
+        posbase = np.array([
+            basefuncs[0](t), basefuncs[1](t), basefuncs[2](t)
+        ])
+        poscurv = np.array([
+            curfuncs[0](t), curfuncs[1](t), curfuncs[2](t)
+        ])
+        p, n, b = frenetFromPfuncs(t, basefuncs[0], basefuncs[1], basefuncs[2])
+
+        transmatrix = np.array(
+            [
+                [_calcCos(p, xb), _calcCos(n, xb), _calcCos(b, xb)],
+                [_calcCos(p, yb), _calcCos(n, yb), _calcCos(b, yb)],
+                [_calcCos(p, zb), _calcCos(n, zb), _calcCos(b, zb)],
+            ]
+        )
+        transmatrix = np.array(
+            [
+                [_calcCos(p, xb), _calcCos(p, yb), _calcCos(p, zb)],
+                [_calcCos(n, xb), _calcCos(n, yb), _calcCos(n, zb)],
+                [_calcCos(b, xb), _calcCos(b, yb), _calcCos(b, zb)],
+            ]
+        )
+
+        rotated = np.matmul(transmatrix, poscurv)
+
+        xnew = np.append(xnew, rotated[0] + posbase[0])
+        ynew = np.append(ynew, rotated[1] + posbase[1])
+        znew = np.append(znew, rotated[2] + posbase[2])
+
+    return (xnew, ynew, znew)
+
 # TODO: create a function for easy visualization of track
